31feba1084aef84687f7bcbe927e2758dd1c64c1
rbecherraz_horde.ch <robinbecherraz2@hotmail.fr>
2023-08-02 16:20:01 +0200

    💄 Improve UI

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ let fps = 2;
 let paused = false;
 let frame = 0;
 let alivePercentage = 10;
-let nCol = 100;
+let nCol = 150;
 let nLine = Math.ceil((ctx.canvas.height / ctx.canvas.width) * nCol)
 let flatTorus = true
 
@@ -55,81 +55,88 @@ $('[data-rule-type="b"]').click((evt) => {
     $(evt.currentTarget).toggleClass('applied')
 })
 
-//////////////////////////// FPS ////////////////////////////
+//////////////////////////// Quantity buttons ////////////////////////////
+$('.quantity-buttons').on('click', (evt) => {
+    const fieldName = evt.target.dataset.field ?? null
+    if(!fieldName) return
+    const field = $(`[name="${fieldName}"]`).get(0)
 
-KEYBOARD.onKey('w', (event, keysPressedAskip) => {
-    if (fps != 250) {
-        fps++;
+    if($(evt.target).hasClass('plus')) {
+        field.stepUp(1)
+        field.dispatchEvent(new Event('change'))
+    }
+    if($(evt.target).hasClass('minus')) {
+        field.stepDown(1)
+        field.dispatchEvent(new Event('change'))
+    }
+})
+
+//////////////////////////// FPS ////////////////////////////
+$('[name="fps"]').on('change', (evt) => {
+    const min = parseInt($(evt.currentTarget).attr('min')) ?? null
+    const max = parseInt($(evt.currentTarget).attr('max')) ?? null
+    if(min && evt.currentTarget.value < min) evt.currentTarget.value = min
+    if(max && evt.currentTarget.value > max) evt.currentTarget.value = max
+    else {
+        fps = evt.currentTarget.value
         if (!paused) {
             clearInterval(timer);
             startTimer()
         }
-        $('#fps').html(fps);
     }
-});
-KEYBOARD.onKey('s', (event, keysPressedAskip) => {
-    if (fps != 1) {
-        fps--;
+})
+
+//////////////////////////// AlivePercentage ////////////////////////////
+$('[name="aliveProb"]').on('change', (evt) => {
+    const min = parseInt($(evt.currentTarget).attr('min')) ?? null
+    const max = parseInt($(evt.currentTarget).attr('max')) ?? null
+    if(min && evt.currentTarget.value < min) evt.currentTarget.value = min
+    if(max && evt.currentTarget.value > max) evt.currentTarget.value = max
+    else {
+        alivePercentage = evt.currentTarget.value
         if (!paused) {
             clearInterval(timer);
             startTimer()
         }
-        $('#fps').html(fps);
     }
-});
+})
+
+//////////////////////////// GRID SIZE ////////////////////////////
+$('[name="tileSize"]').on('change', (evt) => {
+    const min = parseInt($(evt.currentTarget).attr('min')) ?? null
+    const max = parseInt($(evt.currentTarget).attr('max')) ?? null
+    if(min && evt.currentTarget.value < min) evt.currentTarget.value = min
+    if(max && evt.currentTarget.value > max) evt.currentTarget.value = max
+    else {
+        nCol = evt.currentTarget.value
+        nLine = Math.ceil((ctx.canvas.height / ctx.canvas.width) * nCol)
+    }
+})
+
 
 //////////////////////////// Pause ////////////////////////////
-
-KEYBOARD.onKey('p', (event, keysPressedAskip) => {
+$('#playPauseButton').on('click', (evt) => {
     if (paused) {
         startTimer()
+        $(evt.currentTarget).html('Pause')
+        $(evt.currentTarget).css("background-color", "transparent")
         paused = false;
     } else {
         clearInterval(timer);
+        $(evt.currentTarget).html('Play')
+        $(evt.currentTarget).css("background-color", "green")
         paused = true;
     }
-});
+})
 
 //////////////////////////// Reset ////////////////////////////
-
-KEYBOARD.onKey('r', (event, keysPressedAskip) => {
+$('#resetButton').on('click', (evt) => {
     god = new LifeLikeAutomaton(nCol, nLine, flatTorus);
+    frame=0;
     god.populateMatrix(alivePercentage / 100);
     god.draw(ctx);
     god.makeLife(bRule, sRule);
-});
-
-//////////////////////////// AlivePercentage ////////////////////////////
-
-KEYBOARD.onKey('a', (event, keysPressedAskip) => {
-    if (alivePercentage >= 1) {
-        alivePercentage--;
-        $('#aliveProb').html(alivePercentage + '%');
-    }
-});
-KEYBOARD.onKey('d', (event, keysPressedAskip) => {
-    if (alivePercentage < 100) {
-        alivePercentage++;
-        $('#aliveProb').html(alivePercentage + '%');
-    }
-});
-
-//////////////////////////// GRID SIZE ////////////////////////////
-
-KEYBOARD.onKey('z', (event, keysPressedAskip) => {
-    if (nCol < 500) {
-        nCol += 10;
-        nLine = Math.ceil((ctx.canvas.height / ctx.canvas.width) * nCol)
-        $('#tileSize').html(nCol);
-    }
-});
-KEYBOARD.onKey('h', (event, keysPressedAskip) => {
-    if (nCol > 10) {
-        nCol -= 10;
-        nLine = Math.ceil((ctx.canvas.height / ctx.canvas.width) * nCol)
-        $('#tileSize').html(nCol);
-    }
-});
+})
 
 //////////////////////////// GESTION DU CLICK SUR LA GRILLE ////////////////////////////
 
@@ -142,25 +149,35 @@ let getCellClicked = (evt) => {
     };
 }
 
-$('canvas').click((evt) => {
+$('canvas').on("click", (evt) => {
     let mousePos = getCellClicked(evt)
     god.toggleCell(ctx, mousePos.x, mousePos.y)
 })
 
 //////////////////////////// MAP TYPE ////////////////////////////
 
-KEYBOARD.onKey('m', (event, keysPressedAskip) => {
-    god.toggleMapType();
-    flatTorus ^= true;
-    if (flatTorus) $('#map').html('FLAT TORUS');
-    else $('#map').html('CLOSED');
-});
+$('#flatTorusButton').on('click', (evt) => {
+    if(!flatTorus) {
+        god.toggleMapType();
+        flatTorus = true;
+        $(evt.currentTarget).addClass('current')
+        $('#closedButton').removeClass('current')
+    }
+})
+$('#closedButton').on('click', (evt) => {
+    if(flatTorus) {
+        god.toggleMapType();
+        flatTorus = false;
+        $(evt.currentTarget).addClass('current')
+        $('#flatTorusButton').removeClass('current')
+    }
+})
 
 ////////////////////////////
 
-$('#fps').html(fps);
-$('#tileSize').html(nCol);
-$('#aliveProb').html(alivePercentage + '%');
+$('#fps').val(fps);
+$('#tileSize').val(nCol);
+$('#aliveProb').val(alivePercentage);
 if (flatTorus) $('#map').html('FLAT TORUS');
 else $('#map').html('CLOSED');
 setRules(bRule, sRule)
